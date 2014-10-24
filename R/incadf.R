45d083ca07b491c26c5faab2fe29a3c2e38acbae
Erik Bülow <erik.bulow@rcvast.se>
2014-10-24 09:57:26 +0200

    skeleton

---
+++ R/incadf.R
@@ -0,0 +1,12 @@
+
+#' Handle data.frames with INCA-data
+#' 
+#' New class "incadf" that makes sure that all data is is the right format and handled correctly.
+#' 
+#' For a list of all functions within the package see \code{help(package = "incadf")}
+#' 
+#' @author Erik Bulow
+#' @docType package
+#' @name incadf
+NULL
+
---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(incadf)
+
+test_check("incadf")
